b0c239be0abdf0c60699220f462f05250327623b
backalight <antoine.soros@orange.fr>
2018-10-17 10:38:44 +0200

    CONVERT XML

--- app/src/main/java/fr/formation/convertisseur/MainActivity.java
+++ app/src/main/java/fr/formation/convertisseur/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import static java.lang.Double.parseDouble;
 
@@ -21,12 +22,19 @@ public class MainActivity extends AppCompatActivity {
 
     public void convertir(View view) {
 
-        String inputUser = etEuro.getText().toString();
-        Double valueToConvert = parseDouble(inputUser);
-        Double convertValue = valueToConvert * 6.55957;
-        String str = convertValue.toString();
-        etFrf.setText(str);
+
+        if (etEuro.getText().length() > 0) {
+
+            String inputUser = etEuro.getText().toString();
+            Double valueToConvert = parseDouble(inputUser);
+            Double convertValue = valueToConvert * 6.55957;
+            String str = convertValue.toString();
+            etFrf.setText(str);
+        } else {
+            Toast.makeText(this, "ENTREZ UNE VALEUR", Toast.LENGTH_LONG);
+        }
 
 
     }
 }
+
